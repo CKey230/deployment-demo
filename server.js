a03ecf8df110333636fb785c2a5411acdbc75601
Ckey230 <Ckey230@gmail.com>
2022-12-20 19:52:44 -0500

    Schema added

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const vehicleRoutes = require('./controllers/vehicle')
 require('dotenv').config()
-const PORT = process.env.PORT
+
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use('/vehicle', vehicleRoutes)
+
 
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
+
+// database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
 
